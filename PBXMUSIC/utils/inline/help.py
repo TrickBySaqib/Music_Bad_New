25f4866d8e6f6c7f91e09d4cdb0366df192bbcdf
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-06-08 21:02:51 +0530

    Update help.py

--- PBXMUSIC/utils/inline/help.py
+++ PBXMUSIC/utils/inline/help.py
@@ -168,10 +168,10 @@ def help_pannel(_, START: Union[bool, int] = None):
             ],
             [
                 InlineKeyboardButton(
-                    text="👻 ʙᴀᴅ 👻", url=f"https://t.me/ll_BAD_MUNDA_WORLD_ll"
+                    text="👻 ʙᴀᴅ 👻", url=f"https://t.me/STYLISH_FOUT_CHANNEL"
                 ),
                 InlineKeyboardButton(
-                    text="💥 ᴅᴇᴠɪʟ 💥", url=f"https://t.me/ll_PBX_DEVIL_ll"
+                    text="💥 ᴅᴇᴠɪʟ 💥", url=f"https://t.me/OWNER_TBS"
                 ),
             ],
             [
