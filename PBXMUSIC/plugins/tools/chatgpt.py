1cb36567e2db9b00f8196b5d60d92e44682f4487
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-07-23 19:52:50 +0530

    Update chatgpt.py

--- PBXMUSIC/plugins/tools/chatgpt.py
+++ PBXMUSIC/plugins/tools/chatgpt.py
@@ -1,7 +1,11 @@
+import os
+import random
 import time
-import requests
 from PBXMUSIC import app
-
+import requests
+from pyrogram.types import  Message
+from pyrogram.types import InputMediaPhoto
+from BadAPI import api
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
@@ -13,34 +17,16 @@ from pyrogram import filters
     )
 )
 async def chat_gpt(bot, message):
+    
     try:
-        start_time = time.time()
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-
         if len(message.command) < 2:
-            await message.reply_text("Example:\n\n/chatgpt Where is Punjab?")
+            await message.reply_text(
+            "Example:**\n\n/chatgpt Where is golden temple?")
         else:
-            a = message.text.split(" ", 1)[1]
-            response = requests.get(
-                f"https://chatgpt.apinepdev.workers.dev/?question={a}"
-            )
-
-            try:
-                # Check if "results" key is present in the JSON response
-                if "answer" in response.json():
-                    x = response.json()["answer"]
-                    end_time = time.time()
-                    telegram_ping = (
-                        str(round((end_time - start_time) * 1000, 3)) + " ms"
-                    )
-                    await message.reply_text(
-                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @ll_KUNAAL_MUSIC_ll_bot",
-                        parse_mode=ParseMode.MARKDOWN,
-                    )
-                else:
-                    await message.reply_text("No 'results' key found in the response.")
-            except KeyError:
-                # Handle any other KeyError that might occur
-                await message.reply_text("Error accessing the response.")
+            a = message.text.split(' ', 1)[1]
+            r=api.gemini(a)["results"]
+            await message.reply_text(f" {r} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @ll_THE_BAD_BOT_ll ", parse_mode=ParseMode.MARKDOWN)     
     except Exception as e:
-        await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
+        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
+
